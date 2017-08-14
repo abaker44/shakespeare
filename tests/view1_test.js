f97efe0c7d74a4586827c27c990575fd6f2c4614
Aaron Baker <aaron.baker@microfocus.com>
2017-08-14 10:23:08 -0600

    adding testing framework

--- tests/view1_test.js
+++ tests/view1_test.js
@@ -6,10 +6,13 @@ describe('shakespeare.view module', function() {
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
+    it('should ....', inject(function($controller, $scope) {
       //spec body
       var view1Ctrl = $controller('View1Ctrl');
+      var scope = view1Ctrl.scope;
+      
       expect(view1Ctrl).toBeDefined();
+      expect($scope.avgRating).toBe(0);
     }));
 
   });
